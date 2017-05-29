9f6c344b57ce2afcbe6595304528838303b01357
Stewart Scott <stewart42@gmail.com>
2017-05-29 22:50:26 +0200

    simple add form, very raw

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -13,3 +13,25 @@ Template.body.helpers({
     });
   }
 });
+
+
+Template.body.events({
+  'submit .new-log'(event, template) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const clientName = template.find('#clientName').value;
+    const endPoint = template.find('#endPoint').value;
+
+    // Insert a task into the collection
+    Api_logs.insert({
+      timeStamp: new Date(), // current time
+      clientName,
+      endPoint,
+    });
+
+    // Clear form
+    target.text.value = '';
+  },
+});
